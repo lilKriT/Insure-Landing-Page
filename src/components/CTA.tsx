311ed32bbd625cf71add6e65712b09e177ab55a5
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-05 00:31:29 +0100

    Structured CTA.

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section>
+      <div>
+        <h2>Find out more about how we work</h2>
+        <a href="#">How we work</a>
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
