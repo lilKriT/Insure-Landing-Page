e3068440c8abd28c2ba3e976551c6d978a19d6a6
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-05 00:30:23 +0100

    Structured Features.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section>
+      <h2>We’re different</h2>
+      <div>
+        <article>
+          {/* Icon */}
+          <h3>Snappy Process</h3>
+          <p>
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </p>
+        </article>
+        <article>
+          {/* Icon */}
+          <h3>Affordable Prices</h3>
+          <p>
+            We don’t want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </p>
+        </article>
+        <article>
+          {/* Icon */}
+          <h3>People First</h3>
+          <p>
+            Our plans aren’t full of conditions and clauses to prevent payouts.
+            We make sure you’re covered when you need it.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
