ddac325d7f4cf7326b200e4b3841426fdacecce3
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-04 23:25:35 +0100

    Added images. Added styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-blue-300">Starting</div>;
+  return <div className="text-darkViolet font-dm">Starting</div>;
 };
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkViolet: "hsl(256, 26%, 20%)",
+        grayishBlue: "hsl(216, 30%, 68%)",
+        veryDarkViolet: "hsl(270, 9%, 17%)",
+        darkGrayishViolet: "hsl(273, 4%, 51%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        karla: ["Karla", "sans-serif"],
+        dm: ["DM Serif Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
