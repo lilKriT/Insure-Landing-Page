850234feb0fe4a35962b224e55d2fcba426759fc
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-07 23:46:17 +0100

    Working on the absolute image.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import imageDesktop from "../assets/images/image-intro-desktop.jpg";
 
 const Hero = () => {
   return (
-    <section className="flex justify-center bg-darkViolet text-veryLightGray ">
-      <div className="container flex ">
+    <section className="flex justify-center bg-darkViolet text-veryLightGray mb-36">
+      <div className="container flex py-28 ">
         {/* Left Column */}
         <div className="w-1/2 flex flex-col justify-center items-start gap-8 pr-4">
           <div className="border border-veryLightGray w-28" />
@@ -22,8 +22,8 @@ const Hero = () => {
           </a>
         </div>
         {/* Right Column */}
-        <div className="w-1/2">
-          <img src={imageDesktop} alt="" className="" />
+        <div className="w-1/2 outline relative">
+          <img src={imageDesktop} alt="" className="absolute w-full" />
         </div>
       </div>
     </section>
