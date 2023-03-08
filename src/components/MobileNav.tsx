3fd9f79df3b59f18755c895192106cb4555c31c6
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-08 23:33:28 +0100

    Styling mobile nav.

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -9,25 +9,28 @@ const MobileNav = ({ mobileNavVisible, setMobileNavVisible }: IMobileNav) => {
   return (
     <>
       {mobileNavVisible === true && (
-        <nav className="absolute inset-0 top-[80px] z-10 bg-red-400">
-          <menu className="">
+        <nav className="absolute inset-0 top-[80px] z-10 bg-veryDarkViolet text-veryLightGray uppercase text-center text-2xl px-4">
+          <menu className="flex flex-col gap-4 pt-12">
             <li>
-              <a href="#" className="navLink">
+              <a href="#" className="block px-2 py-2 w-full">
                 How we work
               </a>
             </li>
             <li>
-              <a href="#" className="navLink">
+              <a href="#" className="block px-2 py-2 w-full">
                 Blog
               </a>
             </li>
             <li>
-              <a href="#" className="navLink">
+              <a href="#" className="block px-2 py-2 w-full">
                 Account
               </a>
             </li>
             <li>
-              <a href="#" className="navLink featured">
+              <a
+                href="#"
+                className="block px-2 py-2 w-full outline outline-2 outline-veryLightGray"
+              >
                 View plans
               </a>
             </li>
