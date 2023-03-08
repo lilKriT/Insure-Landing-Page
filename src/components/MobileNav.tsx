4db66fc11b5b614997083a8e2642c5fd1ace56f8
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-08 23:48:17 +0100

    Disabled scrolling while mobile nav is open.

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface IMobileNav {
   mobileNavVisible: boolean;
@@ -6,10 +6,19 @@ interface IMobileNav {
 }
 
 const MobileNav = ({ mobileNavVisible, setMobileNavVisible }: IMobileNav) => {
+  useEffect(() => {
+    console.log("haha");
+    if (mobileNavVisible) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [mobileNavVisible]);
+
   return (
     <>
       {mobileNavVisible === true && (
-        <nav className="absolute inset-0 top-[80px] z-10 bg-veryDarkViolet text-veryLightGray uppercase text-center text-2xl px-4 bg-mobileNavPattern bg-no-repeat bg-bottom bg-[length:100vw]">
+        <nav className="absolute inset-0 top-[80px] z-10 bg-veryDarkViolet text-veryLightGray uppercase text-center text-2xl px-4 bg-mobileNavPattern bg-no-repeat bg-bottom bg-[length:100vw] desktop:hidden">
           <menu className="flex flex-col gap-4 pt-12">
             <li>
               <a href="#" className="block px-2 py-2 w-full">
