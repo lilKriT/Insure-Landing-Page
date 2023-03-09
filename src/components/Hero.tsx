776febda5aa034b37f554b5d2d1b25507c20e22f
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-09 22:07:48 +0100

    Working on Hero mobile.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import imageDesktop from "../assets/images/image-intro-desktop.jpg";
 const Hero = () => {
   return (
     <section className="flex justify-center bg-darkViolet text-veryLightGray relative">
-      <div className="container flex py-28 ">
+      <div className="container flex flex-col-reverse desktop:flex-row desktop:py-28 ">
         {/* Left Column */}
         <div className="w-1/2 flex flex-col justify-center items-start gap-8 pr-4">
           <div className="border border-veryLightGray w-28" />
@@ -22,8 +22,12 @@ const Hero = () => {
           </a>
         </div>
         {/* Right Column */}
-        <div className="w-1/2  relative">
-          <img src={imageDesktop} alt="" className="absolute w-full" />
+        <div className="w-full desktop:w-1/2 relative">
+          <img
+            src={imageDesktop}
+            alt=""
+            className="relative desktop:absolute w-full"
+          />
         </div>
       </div>
       {/* The background images */}
