5f34821c06fec2475ead999347da812f98ee8ed1
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-10 23:37:58 +0100

    Added mobile pattern for CTA.

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <section className="flex justify-center text-veryLightGray mb-28 mx-6">
-      <div className="container bg-ctaDesktop bg-no-repeat bg-right bg-darkViolet flex flex-col desktop:flex-row text-center justify-between items-center px-8 desktop:px-16 py-12 gap-12">
+      <div className="container bg-ctaMobile desktop:bg-ctaDesktop bg-no-repeat bg-right-top bg-darkViolet flex flex-col desktop:flex-row text-center justify-between items-center px-8 desktop:px-16 py-12 gap-12">
         <h2 className="text-6xl">
           Find out more
           <br /> about how we work
