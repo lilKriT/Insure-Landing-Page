4c7a37d3fe32226c0088d8fb2388d86d95542e4f
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-10 23:33:25 +0100

    Added mobile patterns in Hero.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="flex justify-center bg-darkViolet text-veryLightGray relative">
       <div className="container flex flex-col-reverse desktop:flex-row desktop:py-28 desktop:mx-6">
         {/* Left Column */}
-        <div className="w-full desktop:w-1/2 flex flex-col justify-center items-center desktop:items-start text-center desktop:text-left gap-8 pr-4 mobile py-32 desktop:py-0 px-6 desktop:px-0">
+        <div className="w-full desktop:w-1/2 flex flex-col justify-center items-center desktop:items-start text-center desktop:text-left gap-8 pr-4 mobile py-32 desktop:py-0 px-6 desktop:px-0 relative">
           <div className="hidden desktop:block border border-veryLightGray w-28" />
           <h1 className="text-5xl desktop:text-7xl">
             Humanizing <br />
@@ -21,6 +21,7 @@ const Hero = () => {
           <a href="#" className="btn btn--primary">
             View plans
           </a>
+          <div className="absolute inset-0 pointer-events-none h-[125%] block desktop:hidden patternOverlayMobile"></div>
         </div>
         {/* Right Column */}
         <div className="w-full desktop:w-1/2 relative">
@@ -39,7 +40,7 @@ const Hero = () => {
         </div>
       </div>
       {/* The background images */}
-      <div className="absolute w-full pointer-events-none outline-red-500 h-[150%] patternOverlay hidden desktop:block"></div>
+      <div className="absolute w-full pointer-events-none h-[150%] patternOverlay hidden desktop:block"></div>
     </section>
   );
 };
